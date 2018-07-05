6ba1477d0ed579e99678a8eb9616153894e5b006
Ruslanwww <oruslano51@gmail.com>
2018-07-05 10:24:45 +0300

    Add barber.erb

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
-	validates :name, presence: true
+	validates :name, presence: true, length: { minimum: 3 }
 	validates :phone, presence: true
 	validates :datestamp, presence: true
 	validates :barber, presence: true
@@ -26,14 +26,21 @@ get '/' do
 end
 
 get '/visit' do
+	@c = Client.new
 	erb :visit
 end
 
 post '/visit' do
 
-	c = Client.new params[:client]
-	c.save
-	
-	erb "<h2>Спасибо, вы записаны</h2>"
+	@c = Client.new params[:client]
+	if @c.save
+		erb "<h2>Спасибо, вы записаны</h2>"
+	else
+		@error = @c.errors.full_messages.first
+		erb :visit
+	end
+end
 
+get '/barber/:id' do
+	erb :barber
 end
